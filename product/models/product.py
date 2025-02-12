0a91dac21d45025d8da07d703033f7af63f45035
Julio Silva <silvajulio@hotmail.com>
2025-02-12 11:09:17 -0500

    Configuração da paginação para exibição de resultados

--- product/models/product.py
+++ product/models/product.py
@@ -2,6 +2,7 @@ from django.db import models
 from product.models import Category
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
