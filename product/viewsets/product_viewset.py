b2205d8432891673b4a0c0f5e0448288b1bc4744
Julio Silva <silvajulio@hotmail.com>
2025-02-17 17:29:57 -0500

    Adicionando o build.yml + Modificações no código

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-
 # from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 # from rest_framework.permissions import IsAuthenticated
 from product.models import Product
@@ -7,8 +6,8 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    #    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    #    permission_classes = [IsAuthenticated]
+    #   authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    #   permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
